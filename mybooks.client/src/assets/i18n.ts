fa7bbcf53605226cd4f34b9a077e62b2fa172ee0
Andrew Corum <14coruma@gmail.com>
2024-10-04 14:29:16 -0400

    Use i18next for home page

---
+++ mybooks.client/src/assets/i18n.ts
@@ -0,0 +1,50 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import Jumbotron from '../components/home/Jumbotron';
+
+i18n
+    .use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+        debug: true,
+        fallbackLng: 'en',
+        interpolation: {
+            // React already does escaping
+            escapeValue: false,
+        },
+        // Using simple hardcoded resources for simple example
+        resources: {
+            en: {
+                translation: {
+                    home: {
+                        j1: {
+                            readMore: 'Read More',
+                            haveFun: 'Have Fun',
+                            explanation: 'Providing tools to meet your reading goals.',
+                            signupBtn: 'Sign up',
+                        },
+                        j2: {
+                            readingCompanion: 'Reading Companion',
+                            explanation: 'blah blah look at this cool example',
+                        },
+                        j3: {
+                            manageLibrary: 'Manage Your Library',
+                            explanation: 'blah blah keep track of your books and progress',
+                        }
+                    }
+                },
+            },
+            es: {
+                translation: {
+                        j1: {
+                            readMore: 'Read More',
+                            haveFun: 'Have Fun',
+                            explanation: 'Providing tools to meet your reading goals.',
+                        },
+                },
+            },
+        },
+    })
+
+export default i18n
